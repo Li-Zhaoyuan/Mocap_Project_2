e5abebde0f1d7a460c078bb92bc7412a76a3a4c2
pohshania <pohshania@gmail.com>
2017-07-05 23:39:04 +0800

    Added Main Menu!

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneName = "";
+    private float timer = 0;
+    private bool startCountdown = false;
+
+    void Update()
+    {
+        if (startCountdown)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+                ChangeSceneOnClick();
+        }
+    }
+
+    public void ChangeSceneOnClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OnClick()
+    {
+        startCountdown = true;
+    }
+}
