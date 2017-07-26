618ac90c289124cb35438dd0e8ca5239e9b490bf
prophet-leong <leongjunxiang1@hotmail.com>
2017-07-26 13:14:59 +0800

    can change the value of the attack damage in script can be useful for different types of hits

--- Assets/Scripts/AttackBox.cs
+++ Assets/Scripts/AttackBox.cs
@@ -5,7 +5,7 @@ public class AttackBox : MonoBehaviour {
 	[SerializeField]
 	Collider mainCollider;
 	[SerializeField]
-	int damage = 0;
+	public int damage = 0;
 	void Start()
 	{
 		Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), mainCollider);
