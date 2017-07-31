40b34359fb1b5aec243fc65a0e157a96dc6d8836
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-07-31 15:18:40 +0800

    adde the test video in, able to play video in unity le

---
+++ Assets/Scripts/PlayAnimatic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayAnimatic : MonoBehaviour {
+
+    public MovieTexture movie;
+
+    RawImage rawImage;
+
+	// Use this for initialization
+	void Start () {
+        rawImage = GetComponent<RawImage>();
+        PlayMovie();
+	}
+
+    void PlayMovie()
+    {
+        rawImage.texture = movie;
+        movie.Play();
+    }
+}
