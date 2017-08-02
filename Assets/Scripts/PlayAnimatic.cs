df74d4cc6b86e260abe3b653e08a6a0b633a4ffa
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-08-02 11:32:20 +0800

    controller and some temp changes

--- Assets/Scripts/PlayAnimatic.cs
+++ Assets/Scripts/PlayAnimatic.cs
@@ -1,22 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayAnimatic : MonoBehaviour {
 
-    public MovieTexture movie;
+    public MovieTexture StartingMovie;
+    public MovieTexture ChampionWin1;
+    public MovieTexture ChampionWin2;
+    public MovieTexture ChallengerWin1;
+    public MovieTexture ChallengerWin2;
 
+    MovieTexture CurrentMovie;
     RawImage rawImage;
+    string nextSceneName = "MainMenu";
 
 	// Use this for initialization
 	void Start () {
-        rawImage = GetComponent<RawImage>();
-        PlayMovie();
+        DontDestroyOnLoad(this);
+        //rawImage = GetComponent<RawImage>();
+        //PlayMovie();
 	}
-
-    void PlayMovie()
+    void Update()
     {
-        rawImage.texture = movie;
-        movie.Play();
+        if(rawImage == null)
+        {
+            rawImage = FindObjectOfType<RawImage>();
+        }
+        if(!CurrentMovie.isPlaying)
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+
+
+    public void PlayStartingMovie(string SceneToTransitTo)
+    {
+        rawImage.texture = StartingMovie;
+        CurrentMovie = StartingMovie;
+        CurrentMovie.Play();
+        nextSceneName = SceneToTransitTo;
+    }
+
+    public void PlayChampionWin(string SceneToTransitTo)
+    {
+        int rand = Random.Range(1, 2);
+        if (rand == 1)
+        {
+            rawImage.texture = ChampionWin1;
+            CurrentMovie = ChampionWin1;
+        }
+        else
+        {
+            rawImage.texture = ChampionWin2;
+            CurrentMovie = ChampionWin2;
+        }
+        CurrentMovie.Play();
+        nextSceneName = SceneToTransitTo;
+    }
+
+    public void PlayChallengerWin(string SceneToTransitTo)
+    {
+        int rand = Random.Range(1, 2);
+        if (rand == 1)
+        {
+            rawImage.texture = ChallengerWin1;
+            CurrentMovie = ChallengerWin1;
+        }
+        else
+        {
+            rawImage.texture = ChallengerWin2;
+            CurrentMovie = ChallengerWin1;
+        }
+        CurrentMovie.Play();
+        nextSceneName = SceneToTransitTo;
     }
 }
