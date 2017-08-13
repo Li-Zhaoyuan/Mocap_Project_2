70340ec28dc2112a30fc0c5004128ab00cae047f
Garygoh884 <garygoh62575886@gmail.com>
2017-08-13 11:08:14 +0800

    Improved Instructions screen. It should follow the previous version of this screen.

---
+++ Assets/Scripts/PageScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PageScript : MonoBehaviour {
+
+    public GameObject[] pages;
+    int currentPage;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentPage = 0;
+        UpdateCurrentPage();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void NextPage()
+    {
+        ++currentPage;
+        if (currentPage >= pages.Length) currentPage = 0;
+
+        UpdateCurrentPage();
+    }
+
+    void UpdateCurrentPage()
+    {
+        for (int i = 0; i < pages.Length; ++i)
+        {
+            pages[i].SetActive(i == currentPage);
+        }
+    }
+}
